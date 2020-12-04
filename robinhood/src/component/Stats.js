0de3d96c42be1a26b5267ddb68a373e2438569d4
Majd Sufian <majdnewsufian@gmail.com>
2020-12-04 20:32:18 +0100

    fetch all the data from  the api using Axios

--- robinhood/src/component/Stats.js
+++ robinhood/src/component/Stats.js
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../style/Stats.css";
+import Axios from "axios";
 
 function Stats() {
-  return <div className="stats">{/* <h1>stats</h1> */}</div>;
+  const [stockData, setstockData] = useState([]);
+
+  const BASE_URL = "https://finnhub.io/api/v1/quote";
+
+  const getStockData = (stock) => {
+    return Axios.get(
+      `${BASE_URL}?symbol=${stock}&token=bv58i9v48v6qnlld1jc0`
+    ).catch((error) => {
+      console.log("error here " + error);
+    });
+  };
+
+  useEffect(() => {
+    let tempStockData = [];
+    let stockList = ["AAPL", "MSFT", "TSLA", "NIO"];
+
+    let promises = [];
+    stockList.map((stock) => {
+      promises.push(
+        getStockData(stock).then((res) => {
+          tempStockData.push({
+            name: stock,
+            ...res.data,
+          });
+        })
+      );
+    });
+
+    Promise.all(promises).then(() => {
+      setstockData(tempStockData);
+    });
+  }, []);
+
+  return (
+    <div className="stats">
+      <div className="stats__container">
+        <div className="stats__header">
+          <p>Stocks</p>
+        </div>
+        <div className="stats__content">
+          <div className="stats__rows">{/* for the current Stocks */}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Stats;
