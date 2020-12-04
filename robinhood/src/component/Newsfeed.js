abe645dd20cf4325ffde2e1cafd6b6927aa8faaf
Majd Sufian <majdnewsufian@gmail.com>
2020-12-04 11:30:31 +0100

    creat the portfolio value

--- robinhood/src/component/Newsfeed.js
+++ robinhood/src/component/Newsfeed.js
@@ -2,7 +2,18 @@ import React from "react";
 import "../style/Newsfeed.css";
 
 function Newsfeed() {
-  return <div className="newsfeed"></div>;
+  return (
+    <div className="newsfeed">
+      <div className="Newsfeed__container">
+        <div className="newsfeed__charSection">
+          <div className="newsfeed__portfolio">
+            <h1>$114,323.66</h1>
+            <p>+$44.63 (+0.04%) Today</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Newsfeed;
